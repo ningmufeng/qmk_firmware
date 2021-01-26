c9c7cf03faa0f4c17d389f975143dcc1d54de796
Leo Deng <myst.dg@gmail.com>
2021-01-26 15:40:45 +0800

    knoboy

---
+++ keyboards/dg/knoboy/knoboy.h
@@ -0,0 +1,16 @@
+#ifndef KNOBOY_H
+#define KNOBOY_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+  K00, K01, K02, K03, \
+  K10, K11, K12, K13, \
+  K20, K21, K22  \
+) { \
+  { K00,   K01,   K02,   K03 }, \
+  { K10,   K11,   K12,   K13 }, \
+  { K20,   K21,   K22,   KC_NO }  \
+}
+
+#endif
